df159beacdd7d6ce8a1b2d4a14c797ae0c641540
Daniel Tse <danieltse@gmail.com>
2023-11-11 14:48:54 +1100

    Init project

---
+++ src/main/java/playground/tech/springbootplayground/SpringBootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package playground.tech.springbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
+	}
+
+}
