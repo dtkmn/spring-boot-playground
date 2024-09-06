b4b8e5392aa5d67088d8373b012895f3dfeb33df
Daniel Tse <danieltse@gmail.com>
2024-09-06 20:45:48 +1000

    Kafka streams

--- src/main/java/playground/tech/springbootplayground/streams/MyTopology.java
+++ src/main/java/playground/tech/springbootplayground/streams/MyTopology.java
@@ -56,7 +56,7 @@ public class MyTopology {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "tweet-streams-app");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
         KafkaStreams streams = new KafkaStreams(topology, props);
