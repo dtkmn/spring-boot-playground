cd71e49a0b00a6304471b288a51432647a2cef38
Daniel Tse <danieltse@gmail.com>
2024-09-29 22:03:18 +1000

    MyTopology updates

--- src/main/java/playground/tech/springbootplayground/streams/MyTopology.java
+++ src/main/java/playground/tech/springbootplayground/streams/MyTopology.java
@@ -1,24 +1,22 @@
 package playground.tech.springbootplayground.streams;
 
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.*;
 import org.apache.kafka.streams.kstream.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Properties;
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@Configuration
+@Component
 public class MyTopology {
 
-    @Bean
-    public Topology buildTopology() {
-        StreamsBuilder builder = new StreamsBuilder();
+    @Autowired
+    public void buildTopology(StreamsBuilder builder) {
+//        StreamsBuilder builder = new StreamsBuilder();
         // Read the topic as stream
         KStream<byte[], String> tweetStream = builder.stream("tweets", Consumed.with(Serdes.ByteArray(), Serdes.String()));
         tweetStream.print(Printed.<byte[], String>toSysOut().withLabel("tweets"));
@@ -27,6 +25,23 @@ public class MyTopology {
         KTable<String, String> symbolsTable =
                 builder.table("crypto-symbols", Consumed.with(Serdes.String(), Serdes.String()));
         symbolsTable.toStream().print(Printed.<String, String>toSysOut().withLabel("crypto-symbols"));
+
+        // Change the key format of tweet stream from byte[]/numeric to String before doing any join
+        KStream<String, String> tweetsRekeyed = tweetStream.selectKey(this::getCurrency);
+        tweetsRekeyed.print(Printed.<String, String>toSysOut().withLabel("tweets-rekeyed"));
+
+//        // join
+        KStream<String, String> joined = tweetsRekeyed.join(symbolsTable,
+                (tweet, symbol) -> String.format("%s - (%s)", tweet, symbol),
+                Joined.with(Serdes.String(), Serdes.String(), Serdes.String()));
+        joined.print(Printed.<String, String>toSysOut().withLabel("joined"));
+
+        // count
+        KTable<String, Long> counts = tweetsRekeyed
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+                .count();
+//        counts.toStream().print(Printed.<String, Long>toSysOut().withLabel("counts"));
+        counts.toStream().to("counts-tweets", Produced.with(Serdes.String(), Serdes.Long()));
 
 
         // 1:N transform
@@ -52,24 +67,63 @@ public class MyTopology {
         // output topic
         merged.to("formatted-tweets", Produced.with(Serdes.ByteArray(), Serdes.String()));
 
-        return builder.build();
+        // Tumbling Windows
+        TimeWindows window = TimeWindows.of(Duration.ofMinutes(5));
+        // Hopping Windows
+//        TimeWindows window = TimeWindows.of(Duration.ofMinutes(5)).advanceBy(Duration.ofMinutes(4));
+        // Session Windows
+//        SessionWindows window = SessionWindows.with(Duration.ofMinutes(5));
+        // Sliding Aggregation Windows
+//        SlidingWindows window =
+//                SlidingWindows.withTimeDifferenceAndGrace(
+//                        Duration.ofMinutes(5), // max time diff between 2 records
+//                        Duration.ofMinutes(5)); // grace period
+        KStream<String, String> redditPosts = builder.stream("reddit-posts", Consumed.with(Serdes.String(), Serdes.String()));
+        KTable<Windowed<String>, Long> count = redditPosts.groupByKey().windowedBy(window).count();
+
+        // write the alerts to a topic
+        count
+                .filter((key, value) -> value == 3)
+                .toStream()
+                .map((windowKey, value) -> createAlert(windowKey, value))
+                .to("alerts", Produced.with(Serdes.String(), Serdes.String()));
+
+//        return builder.build();
+    }
+
+    private static KeyValue<String, String> createAlert(Windowed<String> windowKey, Long value) {
+        String userId = windowKey.key();
+        String alert = String.format("%s has exceeded the 5 minute post limit", userId);
+        return KeyValue.pair(userId, alert);
+    }
+
+//    private String getCurrency(byte[] key, String tweetText) {
+//        List<String> currencies = Arrays.asList("dogecoin", "bitcoin", "ethereum");
+//        for (String currency: currencies) {
+//            if (tweetText.contains(currency)) {
+//                return currency;
+//            }
+//        }
+//        return "";
+//    }
+
+    public List<String> getCurrencies(String tweetText) {
+        List<String> currencies = Arrays.asList("dogecoin", "bitcoin", "ethereum");
+        List<String> words =
+                Arrays.asList(tweetText.replaceAll("[^a-zA-Z ]", "").toLowerCase().trim().split(" "));
+        return words.stream().distinct().filter(currencies::contains).collect(Collectors.toList());
+    }
+
+    public String getCurrency(byte[] key, String tweetText) {
+        List<String> currencies = getCurrencies(tweetText);
+        if (!currencies.isEmpty()) {
+            return currencies.get(0);
+        }
+        return "";
     }
 
     private String enrichEthTweet(String tweet) {
         return tweet;
-    }
-
-    @Bean
-    public KafkaStreams kafkaStreams(Topology topology) {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "tweet-streams-app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-        KafkaStreams streams = new KafkaStreams(topology, props);
-        streams.start();
-        return streams;
     }
 
 }
